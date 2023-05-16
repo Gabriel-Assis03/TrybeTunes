7e52eb304a3d40dfc7c038736921fe9193403cc9
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2023-05-16 16:02:16 -0300

    r1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-class App extends React.Component {
+import Login from './pages/Login';
+import Album from './pages/Album';
+import Profile from './pages/Profile';
+import Search from './pages/Search';
+import Favorites from './pages/Favorites';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
+
+class App extends Component {
   render() {
-    return (<p>TrybeTun es</p>);
+    return (
+      <div>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="" component={ NotFound } />
+        </Switch>
+      </div>
+    );
   }
 }
 
