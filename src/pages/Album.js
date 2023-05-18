6d0d0eb23533e82675d29a4fd0e204c112b8ad67
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2023-05-18 16:25:13 -0300

    r7 full

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -3,11 +3,17 @@ import React, { Component } from 'react';
 // componentes
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends Component {
   constructor() {
     super();
 
+    this.state = {
+      infosAlbum: '',
+      data: '',
+      loadingData: true,
+    };
   }
 
   render() {
@@ -16,11 +22,34 @@ class Album extends Component {
         params: { id },
       },
     } = this.props;
-    const musics = getMusics(id).then((e) => console.log(e));
-    console.log(musics);
+    // getMusics(id).then((e) => console.log(e));
+    getMusics(id)
+      .then((data) => (
+        <div key={ data[0].collectionId }>
+          <h3 data-testid="artist-name">{ data[0].artistName }</h3>
+          <p data-testid="album-name">{ data[0].collectionName }</p>
+        </div>
+      ))
+      .then((infosAlbum) => {
+        this.setState((prev) => ({
+          ...prev,
+          infosAlbum,
+        }));
+      });
+    getMusics(id)
+      .then((data) => {
+        this.setState((prev) => ({
+          ...prev,
+          data,
+          loadingData: false,
+        }));
+      });
+    const { infosAlbum, data, loadingData } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        { infosAlbum }
+        {loadingData || <MusicCard data={ data } />}
       </div>
     );
   }
