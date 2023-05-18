093990fce535e94831b3e1cf06cc9b25b091c626
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2023-05-17 22:38:34 -0300

    r7 começo

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 
 // componentes
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  constructor() {
+    super();
+
+  }
+
   render() {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
+    const musics = getMusics(id).then((e) => console.log(e));
+    console.log(musics);
     return (
       <div data-testid="page-album">
         <Header />
