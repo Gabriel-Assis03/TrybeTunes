792e3e90f957c5ec57e159698d70c755a02e3fc5
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2023-05-18 23:56:38 -0300

    r12 faltando fazer o codigo de tirar a musica dos favoritos a pag de favorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -2,12 +2,51 @@ import React, { Component } from 'react';
 
 // componentes
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: '',
+      loadingData: false,
+    };
+  }
+
   render() {
+    const { data, loadingData } = this.state;
+    getFavoriteSongs()
+      .then((listFav) => {
+        this.setState((prev) => ({
+          ...prev,
+          data: listFav,
+          loadingData: true,
+        }));
+      });
     return (
       <div data-testid="page-favorites">
         <Header />
+        <div>
+          {loadingData
+            ? (
+              data.map((e, index) => {
+                const { trackId, trackName, previewUrl } = e;
+                if (index !== 0) {
+                  return (<MusicCard
+                    key={ trackId }
+                    trackId={ trackId }
+                    trackName={ trackName }
+                    previewUrl={ previewUrl }
+                    favorite={ this.favorite }
+                  />);
+                }
+                return null;
+              })
+            )
+            : <p>Carregando...</p>}
+        </div>
       </div>
     );
   }
